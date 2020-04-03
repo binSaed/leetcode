b4202f3d2371248201013419ebc3a096d3456867
Abdelrahman Saed <abdoo.dev@gmail.com>
2020-04-03 08:00:10 -0700

    Single Number->Accepted

--- src/com/company/thirty_day_leetcoding_challenge/week1/SingleNumber/Solution.java
+++ src/com/company/thirty_day_leetcoding_challenge/week1/SingleNumber/Solution.java
@@ -1,7 +1,38 @@
 package com.company.thirty_day_leetcoding_challenge.week1.SingleNumber;
 
+import java.util.HashMap;
+
 class Solution {
+
     public int singleNumber(int[] nums) {
-        return 0;
+
+        HashMap<Integer, Integer> numberAndappears = new HashMap<>();
+
+        for (int i : nums) {
+            int count = 1;
+            if (numberAndappears.containsKey(i))
+                count += numberAndappears.get(i);//if num exist before increment the value
+
+            numberAndappears.put(i, count);
+        }
+
+        for (int i : nums) if (numberAndappears.get(i) == 1) return i;// if found num appears one return it
+
+        return -1;//if no item appears one time return -1
+    }
+
+    public static void main(String[] args) {
+        //main Method for test only :)
+
+        //Example 1:
+        //
+        //Input: [2,2,1]
+        //Output: 1
+        //Example 2:
+        //
+        //Input: [4,1,2,1,2]
+        //Output: 4
+        Solution s = new Solution();
+        System.out.println(s.singleNumber(new int[]{4, 1, 2, 1, 2}));
     }
 }
