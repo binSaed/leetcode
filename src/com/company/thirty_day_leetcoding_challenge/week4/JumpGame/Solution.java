66bbd1ccac4d09c34e667b7d1a391e36910c1503
Abdelrahman Saed <abdoo.dev@gmail.com>
2020-04-26 02:58:18 +0200

    JumpGame -> accepted

--- src/com/company/thirty_day_leetcoding_challenge/week4/JumpGame/Solution.java
+++ src/com/company/thirty_day_leetcoding_challenge/week4/JumpGame/Solution.java
@@ -2,9 +2,13 @@ package com.company.thirty_day_leetcoding_challenge.week4.JumpGame;
 
 public class Solution {
     public boolean canJump(int[] nums) {
-        int need = nums.length - 1;
-        for (int i = nums.length - 1; i >= 0; i--) if (i+nums[i ]>= need) need=i;
-        return need == 0;
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
     }
 
     public static void main(String[] args) {
